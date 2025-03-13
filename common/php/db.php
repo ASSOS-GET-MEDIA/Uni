511a6b994cdfb1e8ecd1c5593bc0f0db9bc66609
ento <134372499+entocraft@users.noreply.github.com>
2025-03-13 14:37:12 +0100

    Update db.php

--- common/php/db.php
+++ common/php/db.php
@@ -1,8 +1,8 @@
 <?php
-$host = 'univeradb.mysql.db'; // ou l'adresse de ton serveur MySQL
-$dbname = 'univeradb'; // Remplace par le nom de ta base
-$user = 'univeradb'; // Ton utilisateur MySQL
-$password = 'yDgK4fRnsyJnqJPMLaHM7aqkT'; // Ton mot de passe MySQL
+$host = ''; // ou l'adresse de ton serveur MySQL
+$dbname = ''; // Remplace par le nom de ta base
+$user = ''; // Ton utilisateur MySQL
+$password = ''; // Ton mot de passe MySQL
 
 // Créer la connexion
 $conn = new mysqli($host, $user, $password, $dbname);
